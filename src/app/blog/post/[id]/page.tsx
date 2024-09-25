01e1f04faf93c103e7b6995d74fd388f689ec2c6
pekellogg <peyton846@revature.net>
2024-09-24 19:46:16 -0700

    adds display error to user for posts 404 not found

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,12 +1,18 @@
-import { posts } from "@/app/lib/placeholder-data";
-import Post from "@/app/ui/components/posts/Post";
+import { getPosts } from '@/app/lib/data';
+import Post from '@/app/ui/components/posts/Post';
+import { notFound } from 'next/navigation';
 
-export default function Page({params}: {params: {id: string}}) {
-    const post = posts.find((post) => post.id === params.id);
-    return (
-        <>
-            <h1>Post</h1>
-            {post && <Post {...post} />}
-        </>
-    )
+export default async function Page({ params }: { params: { id: string } }) {
+  const posts = await getPosts();
+  const post = posts?.find((post) => post.id === ""); // params.id; empty string will never match any post (to test 404 errors)
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <>
+      <h1>Post</h1>
+      {post && <Post id={post.id} title={post.title} content={post.content} date={post.date}/>}
+    </>)
 }
