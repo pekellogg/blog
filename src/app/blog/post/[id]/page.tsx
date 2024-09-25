97e8e6ad38ac84309a2118a4babc663b8078d92b
pekellogg <peyton846@revature.net>
2024-09-24 19:54:36 -0700

    removes hard code intended to produce 404 errors on blog/post/[id]

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -4,7 +4,7 @@ import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const posts = await getPosts();
-  const post = posts?.find((post) => post.id === ""); // params.id; empty string will never match any post (to test 404 errors)
+  const post = posts?.find((post) => post.id === params.id); // set to empty string to test 404 errors
 
   if (!post) {
     notFound();
